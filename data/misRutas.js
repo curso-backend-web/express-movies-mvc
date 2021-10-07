61ad486f52351afc549431b1fe4278a00abce713
raul g <rglepe@gmail.com>
2021-10-07 14:26:23 +0200

    last version

---
+++ data/misRutas.js
@@ -0,0 +1,8 @@
+export default [
+    {
+        userId: 1,
+        rutas: [
+
+        ]
+    }
+]
--- data/users.js
+++ data/users.js
@@ -1,6 +1,7 @@
 export default [
     {
     username:'peter',
-    password:'pan'
+    password:'pan',
+    routes:[]
     }
 ]
