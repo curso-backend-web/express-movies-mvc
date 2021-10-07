53f6eb10ca1770a99d898c99d4f55465d8864886
raul g <rglepe@gmail.com>
2021-10-07 13:35:14 +0200

    PORT in .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.listen(PORT,()=>console.log(`listening on ${PORT}`));
+app.listen(process.env.PORT,()=>console.log(`listening on ${process.env.PORT}`));
